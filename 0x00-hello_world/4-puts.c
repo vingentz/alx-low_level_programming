be8a24d441338d07cc264fc46ae7e45aa352ba40
Kevin Kariuki <vingentz@gmail.com>
2023-02-16 16:02:14 +0300

    C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - function entry point
+ * prints Programming is like building a multilingual puzzle
+ * then 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
