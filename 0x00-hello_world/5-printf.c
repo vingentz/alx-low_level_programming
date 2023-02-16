0813a2a42bc3c82f85de8dc0bdc6c54996549215
Kevin Kariuki <vingentz@gmail.com>
2023-02-16 16:11:48 +0300

    Using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function entry point
+ * prints with proper grammar, but the outcome is a piece of art,
+ * Return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return (0);
+}
