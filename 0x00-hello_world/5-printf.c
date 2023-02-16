1ce9a072e56e05729bf1bfe543fc5c88f8e8b361
Kevin Kariuki <vingentz@gmail.com>
2023-02-16 16:14:15 +0300

    Using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - main function entry point
  * prints with proper grammar, but the outcome is a piece of art,
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return (0);
+	return (0);
 }
