7f026a4bb67637a1a77b5e1b29f9dbca70700bfd
Kevin Kariuki <vingentz@gmail.com>
2023-02-17 18:22:57 +0300

    Print alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * print alphabet backwards
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter <= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
