657ed490d230db4be1b8ba2deb52c9e3db2487e5
Kevin Kariuki <vingentz@gmail.com>
2023-02-17 17:33:16 +0300

    Print lower alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = "a";
+
+	while (letter <= "z")
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
