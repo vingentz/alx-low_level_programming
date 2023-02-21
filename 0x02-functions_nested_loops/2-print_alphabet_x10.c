830389c2099eff788abce049610591afe03bcfdb
Kevin Kariuki <vingentz@gmail.com>
2023-02-21 14:50:56 +0300

    Function to print alphabet 10 times and a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet ten times
+ * Return void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= '9')
+	{
+		char l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar(l);
+			++l;
+		}
+		++i;
+	}
+	_putchar('\n')
+}
