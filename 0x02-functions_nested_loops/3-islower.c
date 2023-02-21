487aa4f0c1f2744cd42cd076df5aad9941a4648d
Kevin Kariuki <vingentz@gmail.com>
2023-02-21 15:09:06 +0300

    Function to check for lowercae characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,17 @@
 /**
  * _islower - checks for lowercase characters
  * @c: character to be checked
- * Return: 0
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-		else
-			return (0);
 	}
+		else
+		{
+			return (0);
+		}
 	_putchar('\n');
 }
