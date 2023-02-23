a988817030acf38d451a03bd0a346da287c93b18
Kevin Kariuki <vingentz@gmail.com>
2023-02-23 18:13:54 +0300

    Function to check for digits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,26 +1,96 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-//int _putchar (char c);
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
 
-void print_numbers(void);
+int _putchar(char c);
 
-void print_most_numbers(void);
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
 
 int _isupper(int c);
 
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit to be checked
+ * Return: 1 if true, 0 if false
+ */
+
 int _isdigit(int c);
+
+/**
+ * mul - multiplies two integers
+ * @a: one integer
+ * @b: second integer
+ * Return: product of a and b
+ */
 
 int mul(int a, int b);
 
+/**
+ * print_numbers - print 0 to 9
+ */
+
+void print_numbers(void);
+
+/**
+ * print_most_numbers - print 0 to 9, skip 2 and 4
+ */
+
+void print_most_numbers(void);
+
+/**
+ * more_numbers - Print numbers 0 - 14 ten times followed by a new line 
+ */
+
 void more_numbers(void);
+
+/**
+ * print_line - Prints a straight line in on the terminal
+ * @n: number of times the character '_' is printed
+ */
 
 void print_line(int n);
 
+/**
+ * print_diagonal - print character '\' n times
+ * @n: number of characters to draw
+ */
+
 void print_diagonal(int n);
+
+/**
+ * print_square - print a square of given size
+ * @size: size to draw
+ */
 
 void print_square(int size);
 
+/**
+ * print_triangle - print traingle with #'s with given size
+ * @size: size of triangle to draw
+ */
+
 void print_triangle(int size);
+
+/**
+ * print_triangle - print traingle with #'s with given size
+ * @size: size of triangle to draw
+ */
+
+void print_triangle(int size);
+
+/**
+ * print_number = print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n);
 
 #endif
