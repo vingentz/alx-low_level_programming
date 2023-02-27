81f03d5c2951fb3ce40220704a7137729428ec12
Kevin Kariuki <vingentz@gmail.com>
2023-02-27 16:18:54 +0300

    Function to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+void rev_string(char *s)
+{
+	int i, max, half;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last = s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
