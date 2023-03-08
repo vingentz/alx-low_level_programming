7e6da27d7e33e1f0d50953b839b6c3dced3ce147
Kevin Kariuki <vingentz@gmail.com>
2023-03-08 17:25:34 +0300

    Function that returns 1 if the input integer is a prime number else 0

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * divb - check if the number is prime
+ * @n: number
+ * @m: number
+ * Return: 1 or 0
+ */
+
+int divb(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divb(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+
+/**
+ * is_prime_number - prime
+ * @n: integer params
+ * Return: recursion
+ */
+
+int is_prime_number(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divb(3, n));
+	}
+}
