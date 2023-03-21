b9e19210314d57fa7a136f52010b9ca1dd08d9ba
Kevin Kariuki <vingentz@gmail.com>
2023-03-21 16:52:54 +0300

    Definition of new structure Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: Owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
