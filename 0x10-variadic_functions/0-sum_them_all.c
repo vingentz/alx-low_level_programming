46f989104636b667c337e207f2cbf2443d188c2b
Kevin Kariuki <vingentz@gmail.com>
2023-03-23 15:39:37 +0300

    Function to return sum of all its pparameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Sum of all
+ * @n: Integer to add
+ * Return: Total sum
+ */
+
+int sum_them_all(const unsigned int n, ...);
+{
+	int sum = 0;
+	unsigned int i;
+	va_list valist;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+	return (sum);
+}
