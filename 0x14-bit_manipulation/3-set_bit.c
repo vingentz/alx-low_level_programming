7dc236b7b8c89b6a4d663116bdd222a2623666a7
Kevin Kariuki <vingentz@gmail.com>
2023-04-06 18:20:12 +0300

    Function to set bit value ata given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,9 +9,9 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int test = 0;
+	unsigned long int test = 1;
 
-	if (index > (sizeof(unsigned int) * 8))
+	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	test <<= index;
