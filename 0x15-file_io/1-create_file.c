d9239fc51e5b7150c8f27e164b240654def05166
Kevin Kariuki <vingentz@gmail.com>
2023-04-11 17:16:29 +0300

    Function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,11 +20,15 @@ int create_file(const char *filename, char *text_content)
 	newfile  = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile < 0)
 		return (-1);
+
 	for (len = 0; text_content && text_content[len];)
 		len++;
+
 	byt = write(newfile, text_content, len);
+
 	if (byt < 0)
 		return (-1);
 	close(newfile);
+
 	return (-1);
 }
