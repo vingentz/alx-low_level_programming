bca51856e7bc7f1450593cfd9c9ff593491d2bf9
Kevin Kariuki <vingentz@gmail.com>
2023-04-11 17:52:57 +0300

    Function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,33 @@
 #include "main.h"
-
 /**
- * create_file - creates a file
- * @filename: name of file to create
- * @text_content: NULL terminated string to write to file
- * Return: 1 on success else -1
- * -1 if filename is NULL
+ * create_file - creates a newfile
+ * @filename: name of newfile to create
+ * @text_content: content of the newfile to create
+ * Return: 1 if successful, -1 if it failed
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int len;
 	int newfile;
-	int byt;
 
 	if (filename == NULL)
-		return (-1);
-
-	newfile  = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	{
+		return -1;
+	}
+	newfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile == -1)
-		return (-1);
-
-	for (len = 0; text_content && text_content[len];)
-		len++;
-
-	byt = write(newfile, text_content, len);
-
-	if (newfile == -1 || byt == -1)
-		return (-1);
-
+	{
+		return -1;
+	}
+	if (text_content != NULL)
+	{
+		ssize_t len = strlen(text_content);
+		if (write(newfile, text_content, len) != len)
+		{
+			close(newfile);
+			return -1;
+		}
+	}
 	close(newfile);
-
-	return (-1);
+	return 1;
 }
