9c6c1bb4abea3e4b2e94c8c97963f3030898095f
Kevin Kariuki <vingentz@gmail.com>
2023-04-14 17:27:10 +0300

    Program to copy file contents of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,111 +1,109 @@
 #include "main.h"
 
 /**
- * from_to - Reads from and copys to file
- * @copy: content to copy
+ * err_read - file-read error
+ * @s: file where error has started
  */
-
-void from_to(copy_struct *copy)
+void err_read(char *s)
 {
-	copy->rd = 1;
-
-	while (copy->rd)
-	{
-		copy->rd = read(copy->from_file, copy->buffer, 1024);
-		if (copy->rd < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", copy->from);
-			exit(98);
-		}
-		copy->wt = write(copy->to_file, copy->buffer, copy->rd);
-		if (copy->wt < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", copy->to);
-			exit(99);
-		}
-	}
-}
-/**
- * copy_file - copy file contents
- * @copy: file to copy content from
- */
-void copy_file(copy_struct *copy)
-{
-	copy->from_file = open(copy->from, O_RDONLY);
-
-	if (copy->from_file < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", copy->from);
-		exit(98);
-	}
-	copy->to_file = open(copy->to, O_CREAT | O_WRONLY | O_TRUNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (copy->to_file < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Cant write to %s\n", copy->to);
-		exit(99);
-	}
-	from_to(copy);
-	copy->wt = close(copy->from_file);
-	if (copy->wt < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", copy->from_file);
-		exit(100);
-	}
-	copy->wt = close(copy->to_file);
-	if (copy->wt < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file %d\n", copy->to_file);
-		exit(100);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
+	exit(98);
 }
 
 /**
- * init_struct - starts copy process
- * @copy: copy function
- * @check: check statements
+ * read_content - reads a file and puts into a buffer.
+ * @s: source file
+ * @buffer: pointer to buffer
+ * @desc: descriptor
+ * Return: Size of buffer
  */
-void init_struct(copy_struct *copy, char **check)
+
+static ssize_t read_content(char *s, char **buffer, int desc)
 {
-	if (copy == NULL)
-		return;
-	if (check == NULL)
-		return;
-	copy->from = check[1];
-	copy->to = check[2];
-	copy->buffer = malloc(sizeof(char) * 1204);
-	if (copy->buffer == NULL)
-		exit(-1);
+	int read_info;
+
+	if (desc < 0)
+		err_read(s);
+	if (!(*buffer))
+		*buffer = malloc(sizeof(char) * 1024);
+	if (*buffer == NULL)
+		err_read(s);
+	read_info = read(desc, *buffer, 1024);
+	if (read_info < 0)
+	{
+		free(*buffer);
+		err_read(s);
+	}
+	return (read_info);
 }
 /**
- * main -start point
- * @nocheck: number to check arguments
- * @check: check variables
- * Return: 0
+ * cp_err - error handler for copy function
+ * @content: source file to copy content
+ * @buffer: buffer storage
  */
-int main(int nocheck, char **check)
-{
-	copy_struct *cp_command;
 
-	if (nocheck != 3)
+void cp_err(char *content, char *buffer)
+{
+	free(buffer);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", content);
+	exit(99);
+}
+/**
+ * cp - copies file content
+ * @content: file content to copy
+ * @dest: place to paste content
+ * @buffer: buffer
+ * @length: Info length
+ */
+
+static void cp(char *content, int dest, char *buffer, int length)
+{
+	if (dest < 0 || !buffer)
+		cp_err(content, buffer);
+	if (write(dest, buffer, length) < 0)
+		cp_err(content, buffer);
+}
+/**
+ * main - copies file
+ * @ac: arguments count
+ * @cd: cd[1] is copy_from, cd[2] is copy_to
+ * Return: 0 for success
+ */
+
+int main(int ac, char **cd)
+{
+	int original, originalcopy, err, size_of_read;
+	char *buffer, *copy_to, *copy_from;
+
+	buffer = NULL;
+	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (check[1] == NULL)
+	copy_from = cd[1];
+	copy_to = cd[2];
+	original = open(copy_from, O_RDONLY);
+	originalcopy = open(copy_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	for (size_of_read = 1; size_of_read > 0;)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", check[1]);
-		exit(98);
+		size_of_read = read_content(copy_from, &buffer, original);
+		if (!size_of_read)
+		break;
+		cp(copy_to, originalcopy, buffer, size_of_read);
 	}
-	if (check[2] == NULL)
+	free(buffer);
+	err = close(original);
+	if (err < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", check[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", original);
+		exit(100);
 	}
-	cp_command = malloc(sizeof(copy_struct));
-	if (cp_command == NULL)
-		return (0);
-	init_struct(cp_command, check);
-	copy_file(cp_command);
+	err = close(originalcopy);
+	if (err < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", originalcopy);
+		exit(100);
+	}
 	return (0);
 }
