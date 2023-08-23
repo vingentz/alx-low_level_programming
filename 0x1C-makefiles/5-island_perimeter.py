c371e1138d4314ce02c2e045401b13f4c38cce15
Kevin Kariuki <vingentz@gmail.com>
2023-08-23 15:07:48 +0300

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""island perimeter measuring function"""
+
+
+def island_perimeter(grid):
+    """
+    Return island perimeter reped by grid
+    args: grid(list) - a list of lists of integers
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for k in range(height):
+        for l in range(width):
+            if grid[k][l] == 1:
+                size += 1
+                if (l > 0 and grid[k][l - 1] == 1):
+                    edges += 1
+                    if (k > 0 and grid[k - 1][l] == 1):
+                        edges += 1
+    return size * 4 - edges * 2
